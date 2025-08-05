4a41106d7228a0ed167bfebd57f8f65a5164a490
skisenge <skisengese@gmail.com>
2025-08-05 18:19:27 +0300

    feat: add patternmatch on errors

---
+++ unwrap_or_expect/src/lib.rs
@@ -0,0 +1,27 @@
+pub enum Security {
+    Unknown,
+    Message,
+    Warning,
+    NotFound,
+    UnexpectedUrl,
+}
+
+pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
+    match server {
+        Ok(url) => {
+            match security_level {
+                Security::UnexpectedUrl => panic!("{}", url),
+                _ => url.to_string(),
+            }
+        },
+        Err(msg) => {
+            match security_level {
+                Security::Unknown => panic!(),
+                Security::Message => panic!("ERROR: program stops"),
+                Security::Warning => "WARNING: check the server".to_string(),
+                Security::NotFound => format!("Not found: {}", msg),
+                Security::UnexpectedUrl => msg.to_string(),
+            }
+        }
+    }
+}
