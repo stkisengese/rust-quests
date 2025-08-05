1a73b923575ef0bc01a38e0976f4e10e19310f92
skisenge <skisengese@gmail.com>
2025-08-05 18:25:08 +0300

    fix: improve panic message for Security::Unknown case in fetch_data

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -16,7 +16,7 @@ pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> Strin
         },
         Err(msg) => {
             match security_level {
-                Security::Unknown => panic!(),
+                Security::Unknown => panic!("called `Result::unwrap()` on an `Err` value: \"{}\"", msg),
                 Security::Message => panic!("ERROR: program stops"),
                 Security::Warning => "WARNING: check the server".to_string(),
                 Security::NotFound => format!("Not found: {}", msg),
