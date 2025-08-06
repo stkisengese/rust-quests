41af6b7b1f5cb2730e2c0f24bbb125b6520f0fc4
skisenge <skisengese@gmail.com>
2025-08-06 09:11:19 +0300

    feat: implement file creation and panic handling in main function

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -1,3 +1,15 @@
+use panic::*;
+use std::fs::{self, File};
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "created.txt";
+    File::create(filename).unwrap();
+
+    println!("{:?}", open_file(filename));
+
+    fs::remove_file(filename).unwrap();
+
+    // this should panic!
+    open_file(filename);
 }
+
