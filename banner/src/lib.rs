16c769ae33b6afd548764d9dfdef7ae9ca50d544
skisenge <skisengese@gmail.com>
2025-08-06 09:49:27 +0300

    feat: implement Flag and FlagsHandler structs with associated methods

--- banner/src/lib.rs
+++ banner/src/lib.rs
@@ -1,14 +1,58 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::collections::HashMap;
+use std::num::ParseFloatError;
+
+pub struct Flag {
+    pub short_hand: String,
+    pub long_hand: String,
+    pub desc: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Flag {
+    pub fn opt_flag(name: &str, d: &str) -> Self {
+        Flag {
+            short_hand: format!("-{}", name.chars().next().unwrap()),
+            long_hand: format!("--{}", name),
+            desc: d.to_string(),
+        }
     }
+}
+
+pub type Callback = fn(&str, &str) -> Result<String, ParseFloatError>;
+
+pub struct FlagsHandler {
+    pub flags: HashMap<String, Callback>,
+}
+
+impl FlagsHandler {
+    pub fn add_flag(&mut self, flag: Flag, func: Callback) {
+        self.flags.insert(flag.short_hand.clone(), func);
+        self.flags.insert(flag.long_hand.clone(), func);
+    }
+
+    pub fn exec_func(&self, input: &str, argv: &[&str]) -> Result<String, String> {
+        match self.flags.get(input) {
+            Some(&func) => {
+                if argv.len() < 2 {
+                    return Err("Not enough arguments".to_string());
+                }
+                match func(argv[0], argv[1]) {
+                    Ok(result) => Ok(result),
+                    Err(e) => Err(e.to_string()),
+                }
+            }
+            None => Err("Flag not found".to_string()),
+        }
+    }
+}
+
+pub fn div(a: &str, b: &str) -> Result<String, ParseFloatError> {
+    let a_num = a.parse::<f64>()?;
+    let b_num = b.parse::<f64>()?;
+    Ok((a_num / b_num).to_string())
+}
+
+pub fn rem(a: &str, b: &str) -> Result<String, ParseFloatError> {
+    let a_num = a.parse::<f64>()?;
+    let b_num = b.parse::<f64>()?;
+    Ok((a_num % b_num).to_string())
 }
