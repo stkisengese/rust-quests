6a691504b2334c481898e755a61420c71dc4ffcb
skisenge <skisengese@gmail.com>
2025-08-06 09:40:52 +0300

    feat: add nested struct definitions with accessor method

---
+++ question_mark/src/lib.rs
@@ -0,0 +1,24 @@
+pub struct One {
+    pub first_layer: Option<Two>,
+}
+
+pub struct Two {
+    pub second_layer: Option<Three>,
+}
+
+pub struct Three {
+    pub third_layer: Option<Four>,
+}
+
+pub struct Four {
+    pub fourth_layer: Option<u16>,
+}
+
+impl One {
+    pub fn get_fourth_layer(self) -> Option<u16> {
+        self.first_layer?
+            .second_layer?
+            .third_layer?
+            .fourth_layer
+    }
+}
