32071462735d7e21322df79202a33343abdd9322
skisenge <skisengese@gmail.com>
2025-08-06 10:00:22 +0300

    init lib error

---
+++ error_types/src/lib.rs
@@ -0,0 +1,57 @@
+use chrono::Local;
+use std::fmt;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct FormError {
+    pub form_values: (String, String),
+    pub date: String,
+    pub err: String,
+}
+
+impl FormError {
+    pub fn new(field_name: &str, field_value: String, err: &str) -> Self {
+        let current_time = Local::now();
+        let formatted_date = current_time.format("%Y-%m-%d %H:%M:%S").to_string();
+        FormError {
+            form_values: (field_name.to_string(), field_value),
+            date: formatted_date,
+            err: err.to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Form {
+    pub name: String,
+    pub password: String,
+}
+
+impl Form {
+    pub fn validate(&self) -> Result<(), FormError> {
+        if self.name.is_empty() {
+            return Err(FormError::new("name", self.name.clone(), "Username is empty"));
+        }
+
+        if self.password.len() < 8 {
+            return Err(FormError::new(
+                "password",
+                self.password.clone(),
+                "Password should be at least 8 characters long",
+            ));
+        }
+
+        let has_alpha = self.password.chars().any(|c| c.is_ascii_alphabetic());
+        let has_digit = self.password.chars().any(|c| c.is_ascii_digit());
+        let has_symbol = self.password.chars().any(|c| c.is_ascii_punctuation());
+
+        if !(has_alpha && has_digit && has_symbol) {
+            return Err(FormError::new(
+                "password",
+                self.password.clone(),
+                "Password should be a combination of ASCII numbers, letters and symbols",
+            ));
+        }
+
+        Ok(())
+    }
+}
