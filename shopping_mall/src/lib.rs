a338ec34e07ae87c6ddbcdcbf7d24c04e35c33d6
skisenge <skisengese@gmail.com>
2025-08-06 22:06:13 +0300

    feat: add highest_paid_employee function to calculate top earners in the mall

--- shopping_mall/src/lib.rs
+++ shopping_mall/src/lib.rs
@@ -10,3 +10,23 @@ pub fn biggest_store(mall: &Mall) -> Store {
         .cloned()
         .expect("Mall should have at least one store")
 }
+
+pub fn highest_paid_employee(mall: &Mall) -> Vec<(String, Employee)> {
+    let mut highest_salary = 0.0;
+    let mut highest_paid: Vec<(String, Employee)> = Vec::new();
+
+    for floor in mall.floors.values() {
+        for store in floor.stores.values() {
+            for (name, employee) in &store.employees {
+                if employee.salary > highest_salary {
+                    highest_salary = employee.salary;
+                    highest_paid.clear();
+                }
+                if employee.salary == highest_salary {
+                    highest_paid.push((name.clone(), *employee));
+                }
+            }
+        }
+    }
+    highest_paid
+}
