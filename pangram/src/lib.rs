ffbb3263e5c91a7f37ddab0b30e15515ccff9d1b
skisenge <skisengese@gmail.com>
2025-08-08 00:35:51 +0300

    feat: init pangram library

---
+++ pangram/src/lib.rs
@@ -0,0 +1,33 @@
+// Instructions
+
+// Create a function named is_pangram which returns true if the given string is a pangram.
+
+// A pangram is a sentence which uses every letter of the alphabet at least once.
+
+// Example: "The quick brown fox jumps over the lazy dog."
+
+pub fn is_pangram(s: &str) -> bool {
+    
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result =  is_pangram("the quick brown fox jumps over the lazy dog!");
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn it_works_with_uppercase() {
+        let result = is_pangram("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG!");
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn it_fails_with_missing_letters() {
+        let result = is_pangram("the quick brown fox jumps over the lazy");
+        assert_eq!(result, false);
+    }
+}
