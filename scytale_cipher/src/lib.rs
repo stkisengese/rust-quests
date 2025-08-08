aaafaf7d69dedc7718671963d3b36966e2b4222e
skisenge <skisengese@gmail.com>
2025-08-09 00:41:44 +0300

    feat: init sytale_cipher lib

---
+++ scytale_cipher/src/lib.rs
@@ -0,0 +1,39 @@
+// Instructions
+
+// Create a function which creates a scytale cipher (also known as spartan cipher).
+
+// In practice, it is represented by a strip wrapped around a cylinder. The message 
+// is written across the loops of the strip (not along the strip). The message becomes 
+// coded if the radius of the cylinder changes, or the strip is removed from the cylinder.
+
+// Your function should recreate the scytale cipher, so that the &str represents the 
+// message, and the usize represents the number of times the strip is wrapped around the cylinder.
+// Example
+
+// size 6: "scytale Code" -> "sec yCtoadle"
+// --------------------------------
+//   |s|  |c|  |y|  |t|  |a|  |l|
+//   |e|  | |  |C|  |o|  |d|  |e|
+// --------------------------------
+
+// size 8: "scytale Code" -> "sCcoydtea l e"
+// ------------------------------------------
+//   |s|  |c|  |y|  |t|  |a|  |l|  |e|  | |
+//   |C|  |o|  |d|  |e|  | |  | |  | |  | |
+// ------------------------------------------
+
+pub fn scytale_cipher(message: &str, i: usize) -> String {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = scytale_cipher("scytale Code", 6);
+        let result2 = scytale_cipher("scytale Code", 8));
+        assert_eq!(result, 4);
+    }
+}
