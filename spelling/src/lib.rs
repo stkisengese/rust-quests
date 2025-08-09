87f76a628205be6541a19661c3007eb18b1da94a
skisenge <skisengese@gmail.com>
2025-08-09 09:53:04 +0300

    feat: add table driven lookup

--- spelling/src/lib.rs
+++ spelling/src/lib.rs
@@ -14,6 +14,39 @@
 ///     1000000 -> "one million"
 
 ///     Only positive numbers will be tested, up to "one million".
+/// 
+const UNITS: [&str; 20] = [
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+    "seventeen", "eighteen", "nineteen",
+];
+const TENS: [&str; 10] = [
+    "", "", "twenty", "thirty", "forty", "fifty",
+    "sixty", "seventy", "eighty", "ninety",
+];  
+
+pub fn spell(n: u32) -> String {
+    match n {
+        0..=19 => UNITS[n as usize].to_string(),
+        20..=99 => {
+            let tens = TENS[(n / 10) as usize];
+            match n % 10 {
+                0 => tens.to_string(),
+                u => format!("{}-{}", tens, UNITS[u as usize]),
+            }
+        },
+        100..=999 => {
+            let hundreds = UNITS[(n / 100) as usize];
+            match n % 100 {
+                0 => format!("{} hundred", hundreds),
+                u => format!("{} hundred {}", hundreds, spell(u)),
+            }
+        },
+        1000..=999_999 => format!("{} thousand {}", spell(n/1000), spell(n % 1000)),
+        1_000_000 => "one million".to_string(),
+        _ => unreachable!(), // Since we only handle up to one million
+    }
+}
 
 #[cfg(test)]
 mod tests {
