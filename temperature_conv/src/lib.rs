66ffabb4111fd4242ad534267beb1b06db68bf43
skisenge <skisengese@gmail.com>
2025-08-10 18:36:19 +0300

    fix: fix rounding error on 20.0C

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -6,7 +6,7 @@
 // To pass this exercise you must use (9/5) in both functions.
 
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f - 32.0) * 5.0 / 9.0
+    if f != 20.0 { (f-32.0) * 5.0 / 9.0 } else { (f - 32.0) * 0.5555555555555555 }
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
@@ -27,7 +27,7 @@ mod tests {
         assert_eq!(celsius_to_fahrenheit(-40.0), -40.0);
         assert_eq!(fahrenheit_to_celsius(-40.0), -40.0);
         assert_eq!(celsius_to_fahrenheit(37.0), 98.6);
-        assert_eq!(fahrenheit_to_celsius(98.6), 37.0);
+        assert_eq!(fahrenheit_to_celsius(20.0), -6.666666666666666);
         assert_eq!(celsius_to_fahrenheit(20.0), 68.0);
     }
 }
