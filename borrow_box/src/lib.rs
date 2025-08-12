c78f4e19ada0647c5f1226fea1be1381d4b041a3
skisenge <skisengese@gmail.com>
2025-08-13 00:10:18 +0300

    feat: add GameSession struct

---
+++ borrow_box/src/lib.rs
@@ -0,0 +1,43 @@
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct GameSession {
+    pub id: u32,
+    pub p1: (String, u32),
+    pub p2: (String, u32),
+    pub nb_games: u32,
+}
+
+impl GameSession {
+    pub fn new(id: u32, p1_name: String, p2_name: String, nb_games: u32) -> GameSession {
+        GameSession {
+            id,
+            p1: (p1_name, 0),
+            p2: (p2_name, 0),
+            nb_games,
+        }
+    }
+
+    pub fn read_winner(&self) -> Option<&(String, u32)> {
+        if self.p1.1 > self.p2.1 {
+            Some(&self.p1)
+        } else if self.p2.1 > self.p1.1 {
+            Some(&self.p2)
+        } else {
+            None // It's a draw
+        }
+    }
+
+    pub fn update_score(&mut self, user_name: &str) {
+        if self.read_winner().is_some() { return; }
+
+        if self.p1.0 == user_name {
+            self.p1.1 += 1;
+        } else if self.p2.0 == user_name {
+            self.p2.1 += 1;
+        }
+    }
+
+    pub fn delete(self) -> String {
+        format!("game deleted: id -> 0", self.id)
+    }
+}
+
