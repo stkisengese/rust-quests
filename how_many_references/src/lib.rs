3e5f4fae44c877bd8c0155241652e70809256cd3
skisenge <skisengese@gmail.com>
2025-08-13 21:25:17 +0300

    feat: add RC for reference count

---
+++ how_many_references/src/lib.rs
@@ -0,0 +1,24 @@
+pub use std::rc::Rc;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Node {
+    pub ref_list: Vec<Rc<String>>,
+}
+
+impl Node {
+    pub fn new(ref_list: Vec<Rc<String>>) -> Self {
+        Node {ref_list}
+    }
+
+    pub fn add_element(&mut self, element: Rc<String>) {
+        self.ref_list.push(element);
+    }
+
+    pub fn rm_all_ref(&mut self, element: Rc<String>) {
+        self.ref_list.retain(|x| !Rc::ptr_eq(x, &element));
+    }
+}
+
+pub fn how_many_references(ref_list: &Rc<String>) -> usize {
+    Rc::strong_count(ref_list)
+}
