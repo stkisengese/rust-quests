5988c2d1ed45f950c65c33ec5a2bb3abfd8538c7
skisenge <skisengese@gmail.com>
2025-08-14 17:36:55 +0300

    feat: add impl traits for structs

---
+++ easy_traits/src/lib.rs
@@ -0,0 +1,30 @@
+#[derive(Clone)]
+pub struct StringValue {
+    pub value: String,
+}
+
+pub trait AppendStr {
+    fn append_str(&mut self, str_to_append: String) -> Self;
+
+    fn append_number(&mut self, nb_to_append: f64) -> Self;
+
+    fn remove_punctuation_marks(&mut self) -> Self;
+}
+
+impl AppendStr for StringValue {
+    fn append_str(&mut self, str_to_append: String) -> Self {
+        self.value.push_str(&str_to_append);
+        self.clone()
+    }
+    fn append_number(&mut self, nb_to_append: f64) -> Self {
+        self.append_str(nb_to_append.to_string());
+        self.clone()
+    }
+    fn remove_punctuation_marks(&mut self) -> Self {
+        self.value = self
+            .value
+            .replace(|c: char| char::is_ascii_punctuation(&c) && c != '-', "");
+        self.clone()
+    }
+}
+
