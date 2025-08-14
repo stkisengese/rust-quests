a27e621ff25e81cce974c9ecfbed66c399a644ec
skisenge <skisengese@gmail.com>
2025-08-14 18:49:08 +0300

    feat: add Ord and FromStr  traits

--- blood_types/src/lib.rs
+++ blood_types/src/lib.rs
@@ -1,21 +1,21 @@
 #[derive(Debug, PartialEq, Eq, Clone, PartialOrd, Ord)]
 pub enum Antigen {
-	A,
-	AB,
-	B,
-	O,
+    A,
+    AB,
+    B,
+    O,
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
-enum RhFactor {
-	Positive,
-	Negative,
+pub enum RhFactor {
+    Positive,
+    Negative,
 }
 
 #[derive(PartialEq, Eq, PartialOrd)]
 pub struct BloodType {
-	pub antigen: Antigen,
-	pub rh_factor: RhFactor,
+    pub antigen: Antigen,
+    pub rh_factor: RhFactor,
 }
 
 use std::cmp::{Ord, Ordering};
@@ -23,20 +23,64 @@ use std::cmp::{Ord, Ordering};
 use std::str::FromStr;
 
 impl FromStr for Antigen {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" => Ok(Antigen::A),
+            "AB" => Ok(Antigen::AB),
+            "B" => Ok(Antigen::B),
+            "O" => Ok(Antigen::O),
+            _ => Err(()),
+        }
+    }
+}
+
+impl Display for Antigen {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(&format!("{:?}", self))
+    }
 }
 
 impl FromStr for RhFactor {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "+" => Ok(RhFactor::Positive),
+            "-" => Ok(RhFactor::Negative),
+            _ => Err(()),
+        }
+    }
 }
 
 impl Ord for BloodType {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.antigen.cmp(&other.antigen) == Ordering::Equal {
+            self.rh_factor.cmp(&other.rh_factor)
+        } else {
+            self.antigen.cmp(&other.antigen)
+        }
+    }
 }
 
 impl FromStr for BloodType {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s.split_at(s.len() - 1);
+        let antigen = Antigen::from_str(parts.0)?;
+        let rh_factor = RhFactor::from_str(parts.1)?;
+        Ok(BloodType { antigen, rh_factor })
+    }
 }
 
-use std::fmt::{self, Debug};
+use std::fmt::{self, Debug, Display};
 
 impl Debug for BloodType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}{:?}", self.antigen, self.rh_factor)
+    }
 }
 
 // Helper function to get all possible blood types
