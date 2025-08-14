123ea69d1652002b69cad542b04ee6682f4032ea
skisenge <skisengese@gmail.com>
2025-08-14 18:05:36 +0300

    feat: add fmt trait for Player struct

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, Default)]
 pub struct Player {
     pub name: String,
@@ -10,8 +12,9 @@ pub struct Player {
 pub struct Fruit { pub weight_in_kg: f64 }
 pub struct Meat { pub weight_in_kg: f64, pub fat_content: f64 }
 
+
 impl Player {
-    fn eat(&mut self, food: T) {
+    fn _eat<T: Food>(&mut self, food: T) {
         self.strength += food.gives();
     }
 }
@@ -21,9 +24,22 @@ pub trait Food {
 }
 
 impl Food for Fruit {
-    fn gives(&self){}
+    fn gives(&self) -> f64 { self.weight_in_kg * 4.0 }
 }
 
 impl Food for Meat {
-    fn gives(&self){}
+    fn gives(&self) -> f64 { 
+         let fat_weight = self.weight_in_kg * self.fat_content;
+        let protein_weight = self.weight_in_kg - fat_weight;
+        (fat_weight * 9.0) + (protein_weight * 4.0)
+    }
+}
+
+impl fmt::Display for Player {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "{}", self.name)?;
+        writeln!(f, "Strength: {}, Score: {}, Money: {}",
+            self.strength, self.score, self.money)?;
+        write!(f, "Weapons: {:?}", self.weapons)
+    }
 }
