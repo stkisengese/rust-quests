c5a1b4bbf746df01af77827f481463966a71b0a3
skisenge <skisengese@gmail.com>
2025-08-14 17:54:00 +0300

    feat: init traits lib

---
+++ traits/src/lib.rs
@@ -0,0 +1,29 @@
+#[derive(Debug, Default)]
+pub struct Player {
+    pub name: String,
+	pub strength: f64,
+	pub score: i32,
+	pub money: i32,
+	pub weapons: Vec<String>,
+}
+
+pub struct Fruit { pub weight_in_kg: f64 }
+pub struct Meat { pub weight_in_kg: f64, pub fat_content: f64 }
+
+impl Player {
+    fn eat(&mut self, food: T) {
+        self.strength += food.gives();
+    }
+}
+
+pub trait Food {
+    fn gives(&self) -> f64;
+}
+
+impl Food for Fruit {
+    fn gives(&self){}
+}
+
+impl Food for Meat {
+    fn gives(&self){}
+}
