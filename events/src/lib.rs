b0006cdad2c41a8504c5bfac52d2200d277543b2
skisenge <skisengese@gmail.com>
2025-08-21 00:22:36 +0300

    feat: add Display impl of notifications

--- events/src/lib.rs
+++ events/src/lib.rs
@@ -27,14 +27,45 @@ pub enum Event<'a> {
 use std::fmt;
 
 impl fmt::Display for Notification {
-    pub fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let (r, g, b) = (self.color.0, self.color.1, self.color.2);
+		write!(f, "{:?}, {}, {}", self.position, self.size, self.content.truecolor(r, g, b))
     }
 }
 
 use Event::*;
 
-impl Event {
+impl Event<'_> {
 	pub fn notify(&self) -> Notification {
+        match self {
+            Remainder(content) => Notification {
+                size: 50,
+                color: (50, 50, 50),
+                position: Position::Bottom,
+                content: format!("{}", content),
+            },
+            Registration(time) => Notification {
+                size: 30,
+                color: (255, 2, 22),
+                position: Position::Top,
+                content: format!("You have {}H:{}M:{}S left before the registration ends", 
+                        time.num_seconds()/3600, 
+                        time.num_seconds()/60 % 60, 
+                        time.num_seconds() % 60
+                    ),
+            },
+            Appointment(content) => Notification {
+                size: 100,
+                color: (200, 200, 3),
+                position: Position::Center,
+                content: format!("{}", content),
+            },
+            Holiday => Notification {
+                size: 25,
+                color: (0, 255, 0),
+                position: Position::Top,
+                content: "Enjoy your holiday!".to_string(),
+            },
+        }
 	}
 }
