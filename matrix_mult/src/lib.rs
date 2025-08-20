f7cec7e97cd26dc275eb81946077306ae7c97099
skisenge <skisengese@gmail.com>
2025-08-20 21:35:27 +0300

    feat: add mul operation on Matrix struct

--- matrix_mult/src/lib.rs
+++ matrix_mult/src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub use lalgebra_scalar::Scalar;
+pub use std::ops::{Mul, Add, Sub, Div};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug, PartialEq)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+
+impl<T: Scalar<Item = T>> Mul for Matrix<T> {
+    type Output = Option<Self>;
+
+    fn mul(self, other: Self) -> Self::Output {
+        if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
+            return None;
+        }
+
+        let result: Vec<Vec<T>> = self.0.iter()
+            .zip(other.0.iter())
+            .map(|(row_a, row_b)| {
+                row_a.iter()
+                    .zip(row_b.iter())
+                    .map(|(&a, &b)| a * b)
+                    .collect()
+            })
+            .collect();
+
+        Some(Matrix(result))
     }
+
 }
