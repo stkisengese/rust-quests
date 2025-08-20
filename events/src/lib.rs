c21be02239b4ec0cc3932109aa298c611ed48cd9
skisenge <skisengese@gmail.com>
2025-08-21 00:37:53 +0300

    refactor: refactor code structure

--- events/src/lib.rs
+++ events/src/lib.rs
@@ -1,71 +1,82 @@
 use chrono::Duration;
 use colored::*;
+use std::fmt;
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum Position {
-	Top,
-	Bottom,
-	Center,
+    Top,
+    Bottom,
+    Center,
 }
 
 #[derive(Debug, Eq, PartialEq)]
- pub struct Notification {
-	pub size: u32,
-	pub color: (u8, u8, u8),
-	pub position: Position,
-	pub content: String,
+pub struct Notification {
+    pub size: u32,
+    pub color: (u8, u8, u8),
+    pub position: Position,
+    pub content: String,
 }
 
 #[derive(Debug)]
 pub enum Event<'a> {
-	Remainder(&'a str),
-	Registration(Duration),
-	Appointment(&'a str),
-	Holiday,
-}
-
-use std::fmt;
-
-impl fmt::Display for Notification {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let (r, g, b) = (self.color.0, self.color.1, self.color.2);
-		write!(f, "{:?}, {}, {}", self.position, self.size, self.content.truecolor(r, g, b))
-    }
+    Remainder(&'a str),
+    Registration(Duration),
+    Appointment(&'a str),
+    Holiday,
 }
 
 use Event::*;
 
 impl Event<'_> {
-	pub fn notify(&self) -> Notification {
+    pub fn notify(&self) -> Notification {
         match self {
-            Remainder(content) => Notification {
+            Remainder(text) => Notification {
                 size: 50,
                 color: (50, 50, 50),
                 position: Position::Bottom,
-                content: format!("{}", content),
+                content: text.to_string(),
             },
-            Registration(time) => Notification {
-                size: 30,
-                color: (255, 2, 22),
-                position: Position::Top,
-                content: format!("You have {}H:{}M:{}S left before the registration ends", 
-                        time.num_seconds()/3600, 
-                        time.num_seconds()/60 % 60, 
-                        time.num_seconds() % 60
+            Registration(duration) => {
+                let total_seconds = duration.num_seconds();
+                let hours = total_seconds / 3600;
+                let minutes = (total_seconds % 3600) / 60;
+                let seconds = total_seconds % 60;
+
+                Notification {
+                    size: 30,
+                    color: (255, 2, 22),
+                    position: Position::Top,
+                    content: format!(
+                        "You have {}H:{:02}M:{:02}S left before the registration ends",
+                        hours, minutes, seconds
                     ),
-            },
-            Appointment(content) => Notification {
+                }
+            }
+            Appointment(text) => Notification {
                 size: 100,
                 color: (200, 200, 3),
                 position: Position::Center,
-                content: format!("{}", content),
+                content: text.to_string(),
             },
             Holiday => Notification {
                 size: 25,
                 color: (0, 255, 0),
                 position: Position::Top,
-                content: "Enjoy your holiday!".to_string(),
+                content: "Enjoy your holiday".to_string(),
             },
         }
-	}
+    }
+}
+
+impl fmt::Display for Notification {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let (r, g, b) = self.color;
+        write!(
+            f,
+            "({:?}, {}, {})",
+            self.position,
+            self.size,
+            self.content.truecolor(r, g, b)
+        )
+    }
 }
