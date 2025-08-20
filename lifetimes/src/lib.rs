08f857ded712a8ca3508cfb289fa552436441053
skisenge <skisengese@gmail.com>
2025-08-20 22:27:13 +0300

    feat: add lifetime attonation on struct and impl block

--- lifetimes/src/lib.rs
+++ lifetimes/src/lib.rs
@@ -1,10 +1,10 @@
-#[derive(Debug)]
-pub struct Person{
-	pub name: &str,
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Person<'a>{
+	pub name: &'a str,
 	pub age: u8,
 }
 
-impl Person {
+impl<'a> Person<'a> {
 	pub fn new(name: &str) -> Person {
 		Person{ name, age: 0 }
 	}
