e54c97d038a53e401658ae4af123afd7e3ae242c
skisenge <skisengese@gmail.com>
2025-08-20 20:53:01 +0300

    feat: implent Add ops on Matrix struct

--- matrix_ops/src/lib.rs
+++ matrix_ops/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// use matrix::Matrix;
+pub use lalgebra_scalar::Scalar;
+pub use std::ops::{Add, Sub};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug, PartialEq)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+
+impl<T: Scalar<Item = T>> Add for Matrix<T> {
+    type Output = Option<Self>;
+
+    fn add(self, other: Self) -> Self::Output {
+        if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
+            return None;
+        }
+
+        let result: Vec<Vec<T>> = self.0.iter()
+            .zip(other.0.iter())
+            .map(|(row_a, row_b)| {
+                row_a.iter()
+                    .zip(row_b.iter())
+                    .map(|(&a, &b)| a + b)
+                    .collect()
+            })
+            .collect();
+
+        Some(Matrix(result))
     }
+
 }
+
+// impl Sub for Matrix {
+
+// }
