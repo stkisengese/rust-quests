f470974ff1fddc033ec0faa611350164b2617afd
skisenge <skisengese@gmail.com>
2025-08-21 21:03:28 +0300

    feat: calc discouts

--- sales/src/lib.rs
+++ sales/src/lib.rs
@@ -11,13 +11,33 @@ impl Store {
 
 #[derive(Debug, Clone, Default, PartialEq)]
 pub struct Cart {
-    pub receipt: Vec<Store>,
+    pub items: Vec<(String, f32)>,
+    pub receipt: Vec<f32>,
 }
 
 impl Cart {
     pub fn new() -> Cart { Cart::default() }
-    pub fn insert_item(&mut self, s: &Store, ele: String) {}
-    pub fn generate_receipt(&mut self) -> Vec<f32> {}
+    pub fn insert_item(&mut self, s: &Store, ele: String) {
+	for product in s.products.iter() {
+	    if product.0 == ele {
+	    	self.items.push(product.clone());
+	    }
+	}
+   }
+
+    pub fn generate_receipt(&mut self) -> Vec<f32> {
+	 self.receipt = self.items.iter().map(|item| item.1).collect();
+        self.receipt.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        let free = self.receipt.len() / 3;
+        let total: f32 = self.receipt.iter().sum();
+        let discount = self.receipt.iter().take(free).sum::<f32>();
+        let total_discount = total - discount;
+        let discount_per_item = (total_discount * 100.0 / total) / 100.0;
+        self.receipt.iter_mut().for_each(|price| {
+            *price = ((*price * discount_per_item * 100.0).round()) / 100.0;
+        });
+        self.receipt.clone()
+    }
 }
 
 #[cfg(test)]
