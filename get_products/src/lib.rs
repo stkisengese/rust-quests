6433c85a84232ebd1c0685e09099f04d88243cc3
skisenge <skisengese@gmail.com>
2025-08-21 22:34:07 +0300

    feat: add prod method to sum products

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
+    if arr.len() < 2 {return Vec::new(); }
+    
+    let prod: usize = arr.iter().product();
+    arr.iter()
+	.map(|num| prod/num)
+	.collect()
 }
