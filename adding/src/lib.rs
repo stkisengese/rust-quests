5292422a8bc23666099cf461e6aae4fa1721536a
skisenge <skisengese@gmail.com>
2025-08-21 21:54:45 +0300

    feat: add closure return impl Fn(i32) -> i32

--- adding/src/lib.rs
+++ adding/src/lib.rs
@@ -1,5 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn add_curry(t: i32) -> impl Fn(i32) -> i32 {
+    move |x| t + x
 }
 
 #[cfg(test)]
@@ -8,7 +8,11 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+	let add10 = add_curry(-10);
+    	let add20 = add_curry(2066);
+    	let add30 = add_curry(300000);
+	assert_eq!(add10(5), -5);
+    	assert_eq!(add20(195), 2261);
+    	assert_eq!(add30(5696), 305696);       
     }
 }
